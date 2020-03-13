7197b7b737526470d5f5a8f9ed4eda8370f9747e
Marco Siracusa <marco.siracusa93@gmail.com>
2020-03-13 04:48:11 -0700

    deleted scripts having wrong extensions

--- scripts/compile_mt.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-CC ../src/benchmark_mt.cpp -o ../bin/benchmark_mt -fopenmp -O3 -v
--- src/compile_write_mt.cpp
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-CC -c -fPIC libbutils.cpp -o libbutils.o
-ar rcs libbutils.a libbutils.o
-CC ./benchmark_mt.cpp -o ../bin/write_benchmark_mt -fopenmp -O3 -v -L. -lbutils -DENABLE_READ=0 -DENABLE_WRITE=1 -DENABLE_READWRITE=0
